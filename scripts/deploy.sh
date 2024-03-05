46aafb4f561987ba43ad76790946d47531c18465
Saksham Mathur <60032069+sammathur4@users.noreply.github.com>
2024-03-05 16:40:03 +0530

    scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,16 @@
 # Change to the project directory
 
+cd /home/ubuntu/project
 
+#Update the code from the repository
+git pull origin main
 
+# Activate the virtual environment
+source /home/ubuntu/project/venv/bin/activate
+
+# Install dependencies
+pip install -r req.txt
+
+# Apply database migrations
+python /home/ubuntu/project/django_ci_cd/myproject/manage.py makemigrations
+python /home/ubuntu/project/django_ci_cd/myproject/manage.py migrate

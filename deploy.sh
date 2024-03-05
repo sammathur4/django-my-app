c0a0eafffb82a780df9d0bedfd5b0750adbc088a
Saksham Mathur <60032069+sammathur4@users.noreply.github.com>
2024-03-05 16:24:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,4 @@
 ls
 cd /home/ubuntu/project
 
-#Update the code from the repository
-git pull origin main
 
-# Activate the virtual environment
-source /home/ubuntu/project/venv/bin/activate
-
-# Install dependencies
-pip install -r req.txt
-
-# Apply database migrations
-python /home/ubuntu/project/django_ci_cd/myproject/manage.py makemigrations
-python /home/ubuntu/project/django_ci_cd/myproject/manage.py migrate
